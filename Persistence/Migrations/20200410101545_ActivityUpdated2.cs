153a31bee939717a2cdf738d77f466948d3f5b79
Igor Bond <npro27@yandex.ru>
2020-04-11 23:18:58 +1000

    Api complete

---
+++ Persistence/Migrations/20200410101545_ActivityUpdated2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ActivityUpdated2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Deacription",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Deacription");
+        }
+    }
+}

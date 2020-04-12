4dbec1c598f3e2837a266f1124a58065971bb827
Igor Bond <npro27@yandex.ru>
2020-04-12 21:51:10 +1000

    New img and features in client app

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:56591/api/activities")
+      .then((x) => {
+        setActivities(x.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <NavBar></NavBar>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default App;

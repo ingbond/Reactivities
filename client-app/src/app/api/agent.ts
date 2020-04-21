18795b1a2b3bbd212d0ac486b2fc9fbe58f4b898
Igor Bond <npro27@yandex.ru>
2020-04-21 20:16:13 +1000

    complete error handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,27 +1,61 @@
-import { IActivity } from './../models/activity';
-import axios, { AxiosResponse } from 'axios';
+import { IActivity } from "./../models/activity";
+import axios, { AxiosResponse } from "axios";
+import { history } from "../..";
+import { toast } from "react-toastify";
 
-axios.defaults.baseURL = 'http://localhost:56591/api';
+axios.defaults.baseURL = "http://localhost:56591/api";
+axios.interceptors.response.use(undefined, (error) => {
+  console.log("interceptors", error);
+
+  if (error.message === "Network Error" && !error.response) {
+    toast.error("Network Error");
+  }
+  
+  const { status, data, config } = error.response;
+
+  if (status === 404) {
+    history.push("/notfound");
+  }
+
+  if (
+    status === 400 &&
+    config.method === "get" &&
+    data.errors.hasOwnProperty("id")
+  ) {
+    history.push("/notfound");
+  }
+
+  if (status === 500) {
+    toast.error("Server error");
+  }
+});
 
 const responseBoy = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) => new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms))
+const sleep = (ms: number) => (response: AxiosResponse) =>
+  new Promise<AxiosResponse>((resolve) =>
+    setTimeout(() => resolve(response), ms)
+  );
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBoy),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBoy),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBoy),
-    delete: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBoy)
-}
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBoy),
+  post: (url: string, body: {}) =>
+    axios.post(url, body).then(sleep(1000)).then(responseBoy),
+  put: (url: string, body: {}) =>
+    axios.put(url, body).then(sleep(1000)).then(responseBoy),
+  delete: (url: string) =>
+    axios.delete(url).then(sleep(1000)).then(responseBoy),
+};
 
 const Activities = {
-    list: (): Promise<IActivity[]> => requests.get('/activities'),
-    details: (id: string) => requests.get(`/activities/${id}`),
-    create: (activity: IActivity) => requests.post('/activities', activity),
-    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
-    delete: (id: string) => requests.delete(`/activities/${id}`)
-}
+  list: (): Promise<IActivity[]> => requests.get("/activities"),
+  details: (id: string) => requests.get(`/activities/${id}`),
+  create: (activity: IActivity) => requests.post("/activities", activity),
+  update: (activity: IActivity) =>
+    requests.put(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.delete(`/activities/${id}`),
+};
 
 export default {
-    Activities
-}
+  Activities,
+};
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,16 +3,22 @@ import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import { observer } from "mobx-react-lite";
 import ActivityDashboard from "../../features/activties/dashboard/ActivityDashboard";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activties/form/ActivityForm";
 import ActivityDetails from "../../features/activties/details/ActivityDetails";
+import NotFound from "./NotFound";
+import {ToastContainer} from 'react-toastify';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
-
-
   return (
     <Fragment>
+      <ToastContainer position='bottom-right'></ToastContainer>
       <Route exact path="/" component={HomePage}></Route>
       <Route
         path={"/(.+)"}
@@ -20,17 +26,23 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar></NavBar>
             <Container style={{ marginTop: "7em" }}>
-              <Route
-                exact
-                path="/activities"
-                component={ActivityDashboard}
-              ></Route>
-              <Route path="/activities/:id" component={ActivityDetails}></Route>
-              <Route
-                path={["/createActivity", "/manage/:id"]}
-                key={location.key}
-                component={ActivityForm}
-              ></Route>
+              <Switch>
+                <Route
+                  exact
+                  path="/activities"
+                  component={ActivityDashboard}
+                ></Route>
+                <Route
+                  path="/activities/:id"
+                  component={ActivityDetails}
+                ></Route>
+                <Route
+                  path={["/createActivity", "/manage/:id"]}
+                  key={location.key}
+                  component={ActivityForm}
+                ></Route>
+                <Route component={NotFound}></Route>
+              </Switch>
             </Container>
           </Fragment>
         )}

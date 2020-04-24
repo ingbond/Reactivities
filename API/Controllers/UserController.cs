759a65621f2c8c23a4145221711779b45e38af08
Igor Bond <npro27@yandex.ru>
2020-04-24 22:03:57 +1000

    Init Identity

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.User;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(Login.Query query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
